1974b4d025f42edf1d17d5e405c1cc4431b97bea
Denny Huang <denny0223@gmail.com>
2020-07-25 22:25:17 +0800

    Update schedule generator default avatar config

--- src/util/session/config.js
+++ src/util/session/config.js
@@ -3,7 +3,7 @@ var config = {};
 config.spreadsheetKey = "18PleK7TU6zseEnujAp6c9RSb4SkK9PZ2MUfd5N2iMPw";
 // config.spreadsheetKey = "1QsUPjlG5aKsVk5bpCYO4Ssig8MoQmVmd1SCVO_pGrdY"
 
-config.default_avatar = "https://sitcon.org/2020/img/staffs/stone.png";
+config.default_avatar = "https://sitcon.org/2020/img/default_avatar.png";
 config.avatar_base_url = "https://sitcon.org/2020/img/speaker/avatar/";
 
 module.exports = config;
